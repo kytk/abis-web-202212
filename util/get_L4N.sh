e5cd28b4b1cedc52cb115f26bacea93c2166bbf2
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2022-10-23 10:54:53 +0900

    change URL for download

--- util/get_L4N.sh
+++ util/get_L4N.sh
@@ -8,13 +8,15 @@
 # split -n 30 -d L4N-2204-ABiS-20221022.ova L4N-2204-ABiS-split-
 # for f in L4N-2204-ABiS-split-*; do openssl md5 $f > ${f}.md5; done
 # sftp user@ftpsite
-# cd psy-neuroimaging
-# mkdir L4N-2204-ABiS-20221022-split
+# cd www/klab/l4n-abis
+# mkdir L4N-2204-ABiS-split
+# cd L4N-2204-ABiS-split
 # put L4N-2204-ABiS-split-*
-# cd ../L4N #(psy-neuroimaging/L4N)
+# cd .. #www/klab/l4n-abis
 # put L4N-2204-ABiS-20221022*
 #####
 
+#For Debug
 #set -x
 
 cd ~/Downloads
@@ -22,10 +24,10 @@ mkdir L4N-2204-ABiS-20221022
 cd L4N-2204-ABiS-20221022
 
 # variable ################
-baseurl="https://www.md.tsukuba.ac.jp/clinical-med/psy-neuroimaging/L4N-2204-ABiS-20221022-split"
+baseurl="https://www.nemotos.net/l4n-abis/L4N-2204-ABiS-split"
 base="L4N-2204-ABiS-split"
 L4N="L4N-2204-ABiS-20221022.ova"
-L4Nmd5="https://www.md.tsukuba.ac.jp/clinical-med/psy-neuroimaging/L4N/L4N-2204-ABiS-20221022.ova.md5"
+L4Nmd5="MD5(L4N-2204-ABiS-20221022.ova)= 4334c443638c0d432368356122071381"
 ###########################
 
 echo "チュートリアル用のLin4Neuroをダウンロードします"
@@ -34,7 +36,7 @@ echo ""
 echo "${L4N}があるか確認します"
 if [ ! -e ${L4N} ]; then
   echo "L4N分割データを確認し、なければダウンロードします"
-  for n in $(seq -w 00 15);
+  for n in $(seq -w 00 29);
   do
     if [ ! -e ${base}-${n} ]; then
       curl -O ${baseurl}/${base}-${n}.md5
@@ -56,7 +58,7 @@ if [ ! -e ${L4N} ]; then
 fi
 
 echo "${L4N} を検証します"
-curl -O ${L4Nmd5}
+echo ${L4Nmd5} > ${L4N}.md5
 
 openssl md5 ${L4N} | cmp ${L4N}.md5 -
 while [ $? -ne 0 ]; do
