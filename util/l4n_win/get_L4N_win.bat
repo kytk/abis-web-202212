564011c97c7cd2129e854033d20dc13c5a7d6566
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2022-10-29 01:01:07 +0900

    update win

--- util/l4n_win/get_L4N_win.bat
+++ util/l4n_win/get_L4N_win.bat
@@ -7,9 +7,9 @@ echo.
 aria2c -i uris.txt
 
 echo 分割ファイルを結合します
-copy /B L4N-2204-ABIS-split-?? L4N-2204-ABIS-20221026.ova
+copy /B L4N-2204-ABIS-split-?? L4N-2204-ABIS-20221028.ova
 
-echo 完了しました。L4N-2204-ABIS-20221026.ova をVirtualBoxにインポートしてください
+echo 完了しました。L4N-2204-ABIS-20221028.ova をVirtualBoxにインポートしてください
 echo 分割ファイルを削除します
 
 del L4N-2204-ABIS-split*
