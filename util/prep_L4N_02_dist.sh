ffb58c3fddd6d8131437a6ddde559caa84d672d6
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2022-10-30 19:43:48 +0900

    update prep_L4N_02_dist.sh

--- util/prep_L4N_02_dist.sh
+++ util/prep_L4N_02_dist.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Script to prepare split files of L4N for distribution
 
+###
+splitbase="L4N-2004-ABIS-split-"
+###
+
+
 if [ $# -lt 1 ]; then
   echo "Please specify ova files!"
   echo "Usage: $0 OVA"
@@ -9,14 +14,15 @@ fi
 
 vm=$1
 
+cd ~/Documents
 [ ! -d ${vm%.ova} ] && mkdir ~/Documents/${vm%.ova}
 mv $vm ~/Documents/${vm%.ova}
 cd ~/Documents/${vm%.ova}
 chmod 644 $vm
 openssl md5 $vm > ${vm}.md5
-echo "split -n 30 -d ${vm} L4N-2204-ABIS-split-"
-split -n 30 -d ${vm} L4N-2204-ABIS-split-
-for f in L4N-2204-ABIS-split-*; do openssl md5 $f > ${f}.md5; done
+echo "split -n 30 -d ${vm} ${splitbase}"
+split -n 30 -d ${vm} ${splitbase}
+for f in ${splitbase}*; do openssl md5 $f > ${f}.md5; done
 
 echo "Done"
 
@@ -25,9 +31,9 @@ exit
 ####
 # sftp user@ftpsite
 # cd www/klab/l4n-abis
-# put L4N-2204-ABIS-20221028*
-# mkdir L4N-2204-ABIS-20221028-split
-# cd L4N-2204-ABIS-20221028-split
-# put L4N-2204-ABIS-split-*
+# put L4N-2?04-ABIS-2022????.ova*
+# mkdir L4N-2?04-ABIS-2022????-split
+# cd L4N-2?04-ABIS-2022????-split
+# put L4N-2?04-ABIS-split-*
 #####
 
